aede89cbaeaa197121d536351d2955a5487bad76
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-09 13:38:23 -0600

    routes connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Home } from "./pages/Home";
+import Main from "./pages/Main";
+import Currencies from "./pages/Currencies";
+import Price from "./pages/Price";
+import Nav from "./components/Nav";
 
 function App() {
   // const [] = useState(0)
@@ -9,8 +12,11 @@ function App() {
 
   return (
     <>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price/:symbol" element={<Price />} />
       </Routes>
     </>
   );
---
+++ src/pages/Currencies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Currencies = () => {
+  return <h1>Currencies</h1>;
+};
+
+export default Currencies;
